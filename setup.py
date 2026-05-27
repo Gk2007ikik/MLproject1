c0e23761e104af42a314c8f62e9b24e99f9e1027
Gk2007ikik <gkrishgrb@gmail.com>
2026-05-27 12:14:07 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+       requirements = []
+       with open(file_path) as file_obj:                
+            requirements = file_obj.readlines()
+            requirements = [req.replace("\n","") for req in requirements]
+            if "-e ." in requirements:
+                requirements.remove("-e .")
+       return requirements
+setup(
+    name='MLPROJECT',
+    version='0.0.1',
+    author = 'Goutham',
+    author_email='gkrishgrb@gmail.com',
+    packages= find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
